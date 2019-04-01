6fb21470c1896c7875358b9b1205cf97708120a7
Emanuel Pires <emanuelpires11@gmail.com>
2019-04-01 16:25:14 -0400

    searchpage created

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -6,6 +6,40 @@ function search() {
   return (
     <div>
       <Navbar />
+      <div className="container  ">
+        <div className="center-align">
+          <h2>Toolit</h2>
+          <p className="flow-text center-align">
+            When it’s time to buy, rent instead. Toolit connects locals to the
+            tools they need to build and create.&nbsp;
+          </p>
+        </div>
+        <div className="row">
+          <div className="autocomplete col s5">
+            <input
+              type="text"
+              id="toolSearch"
+              placeholder="What tools do you need"
+            />
+
+            <div className="dialog" />
+          </div>
+
+          <div className="autocomplete2 col s5">
+            <input
+              type="text"
+              id="locationSearch"
+              placeholder="Search by location"
+            />
+          </div>
+          <div className="col s2">
+            <button className="btn waves-effect waves-light" id="searchBtn">
+              Search
+            </button>
+          </div>
+        </div>
+        <div />
+      </div>
     </div>
   );
 }
