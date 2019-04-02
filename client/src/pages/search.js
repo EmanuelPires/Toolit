86b6ff7b6aa967e0e45da707e64bdc0575f1179a
Emanuel Pires <emanuelpires11@gmail.com>
2019-04-02 14:36:47 -0400

    working on autocomplete

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -1,46 +1,48 @@
 import React, { Component } from "react";
 import Navbar from "../components/Navbar";
 
-function search() {
-  return (
-    <div>
-      <Navbar />
-      <div className="container  ">
-        <div className="center-align">
-          <h2>Toolit</h2>
-          <p className="flow-text center-align">
-            When it’s time to buy, rent instead. Toolit connects locals to the
-            tools they need to build and create.&nbsp;
-          </p>
-        </div>
-        <div className="row">
-          <div className="autocomplete col s5">
-            <input
-              type="text"
-              id="toolSearch"
-              placeholder="What tools do you need"
-            />
+class Search extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="container  ">
+          <div className="center-align">
+            <h2>Toolit</h2>
+            <p className="flow-text center-align">
+              When it’s time to buy, rent instead. Toolit connects locals to the
+              tools they need to build and create.&nbsp;
+            </p>
+          </div>
+          <div className="row">
+            <div className="autocomplete col s5">
+              <input
+                type="text"
+                id="toolSearch"
+                placeholder="What tools do you need"
+              />
 
-            <div className="dialog" />
-          </div>
+              <div className="dialog" />
+            </div>
 
-          <div className="autocomplete2 col s5">
-            <input
-              type="text"
-              id="locationSearch"
-              placeholder="Search by location"
-            />
+            <div className="autocomplete2 col s5">
+              <input
+                type="text"
+                id="locationSearch"
+                placeholder="Search by location"
+              />
+            </div>
+            <div className="col s2">
+              <button className="btn waves-effect waves-light" id="searchBtn">
+                Search
+              </button>
+            </div>
           </div>
-          <div className="col s2">
-            <button className="btn waves-effect waves-light" id="searchBtn">
-              Search
-            </button>
-          </div>
+          <div />
         </div>
-        <div />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default search;
+export default Search;
