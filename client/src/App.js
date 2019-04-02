6242b7bfde803f094c6e759478df0e46f5e2c217
Michael Fox <mikefox17@gmail.com>
2019-04-01 21:00:19 -0400

    order history workingish

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Login from "./pages/login";
 import Search from "./pages/search";
 import "./App.css";
 import MyProfile from "./pages/myProfile";
-import MyOrders from "./pages/myOrders";
+import myOrders from "./pages/myOrders";
 import MyProducts from "./pages/MyProducts";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
@@ -15,7 +15,7 @@ class App extends Component {
           <Route exact path="/" component={Login} />
           <Route exact path="/search" component={Search} />
           <Route exact path="/myProfile" component={MyProfile} />
-          <Route exact path="/myOrders" component={MyOrders} />
+          <Route exact path="/myOrders" component={myOrders} />
           <Route exact path="/MyProducts" component={MyProducts} />
         </div>
       </Router>
--- client/src/pages/myOrders.js
+++ client/src/pages/myOrders.js
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
 import Navbar from "../components/Navbar";
+import Orders from "../components/Orders";
+import Orderhistory from "../Orderhistory.json";
 // import "./style.css";
 
-function MyOrders() {
-  return (
-    <div>
-      <Navbar />
-    </div>
-  );
+class MyOrders extends Component {
+  state = {
+    Orderhistory
+  };
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="container">
+          <div className="card">
+            <h1>TESTING 123</h1>
+          </div>
+          <div>
+            {this.state.Orderhistory.map(Orderhistory => (
+              <Orders
+                image={Orderhistory.image}
+                item={Orderhistory.item}
+                productID={Orderhistory.productID}
+                OrderDate={Orderhistory.OrderDate}
+                OrderQuantity={Orderhistory.OrderQuantity}
+                OrderCost={Orderhistory.OrderCost}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MyOrders;
