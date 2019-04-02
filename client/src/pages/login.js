2c3cff8fcf3d49a29aeabbbeaa6c9aef567afa53
Michael Fox <mikefox17@gmail.com>
2019-04-02 16:15:05 -0400

    we cant use this login modal

---
+++ client/src/pages/login.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import UserLogin from "../components/UserLogin";
+
+function login() {
+  return (
+    <div className="container">
+      <UserLogin />
+    </div>
+  );
+}
+
+export default login;
