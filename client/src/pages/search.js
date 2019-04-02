34dc063e21e264d007a5d2aeafce860dcb8d5545
Emanuel Pires <emanuelpires11@gmail.com>
2019-04-02 16:30:35 -0400

    first working version of google places autocomplete in search page

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -1,9 +1,29 @@
 import React, { Component } from "react";
 import Navbar from "../components/Navbar";
+import GoogleMapLoader from "react-google-maps-loader";
 import GooglePlacesSuggest from "react-google-places-suggest";
 
-class Search extends Component {
+const MY_API_KEY = "AIzaSyB_aSR45DHCAraJSCrm20csNj_X4LG6410";
+
+export default class Search extends Component {
+  state = {
+    search: "",
+    value: ""
+  };
+  handleInputChange = e => {
+    this.setState({ search: e.target.value, value: e.target.value });
+  };
+
+  handleSelectSuggest = (geocodedPrediction, originalPrediction) => {
+    console.log(geocodedPrediction, originalPrediction); // eslint-disable-line
+    this.setState({ search: "", value: geocodedPrediction.formatted_address });
+  };
+
+  handleNoResult = () => {
+    console.log("No results for ", this.state.search);
+  };
   render() {
+    const { search, value } = this.state;
     return (
       <div>
         <Navbar />
@@ -27,10 +47,41 @@ class Search extends Component {
             </div>
 
             <div className="autocomplete2 col s5">
-              <input
-                type="text"
-                id="locationSearch"
-                placeholder="Search by location"
+              <GoogleMapLoader
+                params={{
+                  key: MY_API_KEY,
+                  libraries: "places,geocode"
+                }}
+                render={googleMaps =>
+                  googleMaps && (
+                    <GooglePlacesSuggest
+                      googleMaps={googleMaps}
+                      autocompletionRequest={{
+                        input: search
+                        // Optional options
+                        // https://developers.google.com/maps/documentation/javascript/reference?hl=fr#AutocompletionRequest
+                      }}
+                      // Optional props
+                      onNoResult={this.handleNoResult}
+                      onSelectSuggest={this.handleSelectSuggest}
+                      textNoResults="My custom no results text" // null or "" if you want to disable the no results item
+                      customRender={prediction => (
+                        <div className="customWrapper">
+                          {prediction
+                            ? prediction.description
+                            : "My custom no results text"}
+                        </div>
+                      )}
+                    >
+                      <input
+                        type="text"
+                        value={value}
+                        placeholder="Search a location"
+                        onChange={this.handleInputChange}
+                      />
+                    </GooglePlacesSuggest>
+                  )
+                }
               />
             </div>
             <div className="col s2">
@@ -46,4 +97,4 @@ class Search extends Component {
   }
 }
 
-export default Search;
+//
