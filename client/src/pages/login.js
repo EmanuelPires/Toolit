b5a95707ddf677cd07722b0f9566a29f0e3ffd3d
Rvijay <rachit.vijay@gmail.com>
2019-04-05 12:24:28 -0400

    Test page

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,12 +1,91 @@
 import React, { Component } from "react";
-import UserLogin from "../components/UserLogin";
+import Test from "../components/Test";
+import API from "../utils/API";
+import axios from "axios";
 
-function login() {
-  return (
-    <div className="container">
-      <UserLogin />
-    </div>
-  );
-}
 
-export default login;
+
+// function Login() {
+
+//   componentDidMount() {
+//     this.loadcustomer();
+//   }
+
+//   loadcustomer = () =>{
+//     API.getcustomer()
+//     .then(res =>
+//       console.log(res))
+//   } 
+
+//   render(){
+//   return(
+//     <div>
+//       <UserLogin />
+//     </div>
+//   )
+//   }
+// };
+
+
+export default class Login extends Component {
+
+  state = {
+    customer: []
+  }
+
+
+  loadcustomer = () =>{
+    API.getcustomer()
+    .then(res =>
+      this.setState({customer: res.data})
+      )
+  } ;
+
+  render(){
+    return (
+      <div className="container">
+        <Test name={this.customer}/>
+        <button onClick={this.loadcustomer} >Test</button>
+      </div>
+    );
+
+  }
+  }
+
+
+// export default class Login extends Component {
+
+//   render(){
+//     return (
+//       componentDidMount() {
+//     this.loadcustomer();
+//   }
+
+//   loadcustomer = () =>{
+//     API.getcustomer()
+//     .then(res =>
+//       console.log(res))
+//   }
+//     );
+//   }
+
+//   // state = {
+
+
+//   // };
+
+  
+
+// };
+
+
+
+// function login() {
+//   return (
+//     <div className="container">
+//       <UserLogin />
+//     </div>
+//   );
+// }
+
+
