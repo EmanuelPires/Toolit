856ab38a978f8525fe032c8d83adc56064771ce9
Michael Fox <mikefox17@gmail.com>
2019-04-08 13:57:01 -0400

    basic profile page

--- client/src/pages/myProfile.js
+++ client/src/pages/myProfile.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Navbar from "../components/Navbar";
+import Profile from "../components/Profile";
 // import "./style.css";
 
 function myProfile() {
   return (
     <div>
       <Navbar />
+      <Profile />
     </div>
   );
 }
