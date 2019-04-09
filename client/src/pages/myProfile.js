c48cf8b4c73443ca224f01de11ae723d7f4058b3
Michael Fox <mikefox17@gmail.com>
2019-04-08 20:08:17 -0400

    merging

--- client/src/pages/myProfile.js
+++ client/src/pages/myProfile.js
@@ -1,24 +1,13 @@
 import React, { Component } from "react";
 import Navbar from "../components/Navbar";
-<<<<<<< HEAD
 import Profile from "../components/Profile";
 // import "./style.css";
-=======
-import Summery from "../components/Summary";
-import Testimony from "../components/Testimony";
-// import "../css/style.css";
->>>>>>> 8371c50fce97040247af12b63420f72bd255905c
 
 function myProfile() {
   return (
     <div>
       <Navbar />
-<<<<<<< HEAD
       <Profile />
-=======
-      <Summery />
-      <Testimony />
->>>>>>> 8371c50fce97040247af12b63420f72bd255905c
     </div>
   );
 }
