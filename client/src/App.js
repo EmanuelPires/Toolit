8700d33d8fc7743146f958aafdfbd4c44b76c897
Michael Fox <mikefox17@gmail.com>
2019-04-09 21:18:10 -0400

    testing auth OKTA

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,54 @@
+//IMPORT CLEANUP
+
 import React, { Component } from "react";
-import Login from "./pages/login";
-import Search from "./pages/search";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Security, SecureRoute, ImplicitCallback } from "@okta/okta-react";
+
+//CSS
 import "./App.css";
+
+//Pages
+import Home from "./pages/Home";
+import Search from "./pages/search";
 import MyProfile from "./pages/myProfile";
 import myOrders from "./pages/myOrders";
 import MyProducts from "./pages/myProducts";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+
+//Components
+import Navbar from "./components/Navbar";
+import Login from "./components/auth/Login";
+
+function onAuthRequired({ history }) {
+  history.push("/login");
+}
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/search" component={Search} />
-          <Route exact path="/myProfile" component={MyProfile} />
-          <Route exact path="/myOrders" component={myOrders} />
-          <Route exact path="/MyProducts" component={MyProducts} />
-        </div>
+        <Security
+          issuer="https://dev-150097.okta.com/oauth2/default"
+          client_id="0oafziqe6tzWiJjt4356"
+          redirect_uri={window.location.origin + "/implicit/callback"}
+          onAuthRequired={onAuthRequired}
+        >
+          {/* Navbar for quick testing secure routes */}
+          <Navbar />
+          <div className="App">
+            <Route path="/" exact={true} component={Home} />
+            <SecureRoute path="/search" exact={true} component={Search} />
+            <Route path="/myProfile" exact={true} component={MyProfile} />
+            <Route path="/myOrders" exact={true} component={myOrders} />
+            <Route path="/MyProducts" exact={true} component={MyProducts} />
+
+            {/* Reroute not working, widget not loading */}
+            <Route
+              path="/login"
+              render={() => <Login baseUrl="https://dev-150097.okta.com" />}
+            />
+            <Route path="/implicit/callback" component={ImplicitCallback} />
+          </div>
+        </Security>
       </Router>
     );
   }
